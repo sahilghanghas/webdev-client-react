706f26c11a901bacd04332b68c3857eded30d7de
sahil <sahilghanghas93@gmail.com>
2019-08-29 15:33:39 -0700

    course components added

---
+++ src/components/WhiteBoard.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import CourseCard from "./CourseCards";
+
+class WhiteBoard extends Component {
+	render() {
+		return (
+			<div>
+				<h1>White Board</h1>
+				<CourseCard/>
+			</div>
+		)
+	}
+}
+
+export default WhiteBoard;
